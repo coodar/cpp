27e08f61fd74d58aba6525560fa611d5e172de5e
coodar <coodar@gmail.com>
2019-07-14 00:18:11 +0800

    wsl

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,18 +1,8 @@
- #include <iostream>
- #include <vector>
- #include <string>
+#include <iostream>
 
- using namespace std;
+using namespace std;
 
- int main()
- {
-
-     vector<string> msg {"Hello", "C++", "World", "from", "VS Code!", "and the C++ extension!"};
-     int i = 0;
-     for (const string &word : msg)
-     {
-         ++i;
-         cout << word << " " << &word <<endl;
-     }
-     cout << endl;
- }
+int main()
+{
+    cout << "hello world" << endl;
+}
